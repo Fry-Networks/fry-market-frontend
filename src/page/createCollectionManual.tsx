5aec887e8b4ca089d9127a8fdb5167179289ce7e
SherazAlam <70172520+SherazAlam@users.noreply.github.com>
2024-08-26 15:38:01 +0500

    integrated create collection api

--- src/page/createCollectionManual.tsx
+++ src/page/createCollectionManual.tsx
@@ -1,19 +1,21 @@
 // @ts-ignore
+import { useWallet } from "@txnlab/use-wallet";
+import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import nft1 from "../assets/images/createNft/profilepic.png";
 import Button from "../components/shared/button";
 import Input from "../components/shared/input";
 import Textarea from "../components/shared/textarea";
-
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
 const CreateNftCollectionManual = () => {
   const [prevImage, setPrevImage] = useState("")
   const navigate = useNavigate();
+  const { activeAccount } = useWallet()
 
   const [formData, setFormData] = useState({
-    collectionName: '',
-    collectionDescription: '',
-    email: '',
+    collection_name: '',
+    description: '',
   });
   const handleInput = (e: any) => {
     console.log(e.target.files[0])
@@ -29,6 +31,62 @@ const CreateNftCollectionManual = () => {
     }));
   };
 
+  const validation = () => {
+    if (formData.collection_name && formData.description) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
+
+  const uploadImage = async () => {
+    try {
+      if (validation()) {
+
+        const formDataForImage = new FormData;
+        formDataForImage.append("images", prevImage);
+        const response = await axios.post(`${baseUrl}/upload-images`, formDataForImage);
+        console.log("Response in upload Image", response.data);
+        setFormData(prev => ({ ...prev, image_url: response.data?.image_urls[0] }))
+        if (response.data?.image_urls[0]) {
+
+          handleContinue(response.data?.image_urls[0])
+        }
+        else {
+          console.log("Some Error Occured while uploading image. Please try again.");
+
+        }
+      }
+      else {
+        console.log("Please provide all information.");
+
+      }
+    }
+    catch (e) {
+      console.log("Error Uploading Image", e);
+
+
+    }
+  }
+
+  const handleContinue = async (imageUrl: any) => {
+    try {
+      const response: any = await axios.post(`${baseUrl}/create-collection`, { ...formData, image_url: imageUrl, collection_address: activeAccount });
+      console.log("Hehe", response.data);
+
+    }
+    catch (e) {
+      console.log("Error Creating Collection");
+
+    }
+  }
+
+  // useEffect(() => {
+  //   console.log("hahs");
+
+  //   handleContinue()
+  // }, [])
 
   return (
     <>
@@ -74,8 +132,8 @@ const CreateNftCollectionManual = () => {
                         label="Collection Name*"
                         placeholder="Name your Collection"
                         className="w-full input-nft"
-                        name="collectionName"
-                        value={formData.collectionName}
+                        name="collection_name"
+                        value={formData.collection_name}
                         onChange={handleChange}
                       />
                     </div>
@@ -87,7 +145,7 @@ const CreateNftCollectionManual = () => {
                         className="w-full input-nft"
                       />
                     </div>
-                    <div>createNftCollection.tsx:99 hello
+                    <div>
 
 
 
@@ -106,13 +164,20 @@ const CreateNftCollectionManual = () => {
                         }
                         rows={6}
                         placeholder="Provide a detailed description of your item"
+                        name="description"
+                        value={formData.description}
+                        onChange={handleChange}
                       />
                     </div>
                     <div className="flex justify-end">
                       <Button
                         className="btn-primary px-8 py-4 mb-5"
                         text="Continue"
-                        onClick={(e: any) => { e.preventDefault(); console.log("hello"); navigate("/select-nft") }}
+                        onClick={(e: any) => {
+                          e.preventDefault(); console.log("hello");
+                          //  navigate("/select-nft") 
+                          uploadImage();
+                        }}
                       />
                     </div>
                   </form>
