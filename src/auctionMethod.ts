3317a3541130f71b33c5b9953522fdeba9273ea6
anwaar001 <94699966+anwaar001@users.noreply.github.com>
2024-09-20 16:45:00 +0500

    change auction address

--- src/auctionMethod.ts
+++ src/auctionMethod.ts
@@ -8,7 +8,7 @@ import { FryAuctionClient } from "./contracts/FryAuction";
 import { FryAuctionBiddingClient } from './contracts/FryAuctionBidding';
 import { getAlgodConfigFromViteEnvironment } from './utils/network/getAlgoClientConfigs';
 
-const AUCTION_ID: bigint = 722263850n;
+const AUCTION_ID: bigint = 722439728n;
 const AUCTION_ADDRESS: string = algosdk.getApplicationAddress(AUCTION_ID)
 const FEE_PERCENT: number = 5000;  // 100 represent 1% & 10000 represent 100%
 const ROYALTY_BASIS: number = 1000;  // 100 represent 1% & 10000 represent 100%
