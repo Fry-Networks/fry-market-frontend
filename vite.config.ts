cc5e9174a2b77e7e2e33bd9d6e1230cedf82f7ae
Octaloop-Technologies <141820308+Octaloop-Technologies@users.noreply.github.com>
2025-12-15 10:52:28 +0500

    verley disable

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     host: '0.0.0.0', // allow external access
     allowedHosts: ['0.0.0.0', 'localhost', 'www.fry.market', 'fry.market'],
+    hmr: { overlay: false }
   },
 })
